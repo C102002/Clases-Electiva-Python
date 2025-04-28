89f2e75c9b109199b7a28a44715cb9d6db20c002
C102002 <alfnicolasfff@hotmail.com>
2025-04-27 22:02:53 -0400

    feat: :sparkles: add type hits

---
+++ clase_2_type_hints/ejemplo_3d.py
@@ -0,0 +1,7 @@
+from typing import TypedDict
+
+class Leader(TypedDict):
+    name: str
+    year: int
+
+author: Leader = {"name": "Gregorio Castillo", "year": 2077}
---
+++ clase_2_type_hints/ejemplo_3e.py
@@ -0,0 +1,31 @@
+from typing import Protocol
+
+class SuperHero(Protocol):
+    def fly(self) -> None:
+        pass
+
+# Esta función acepta cualquier objeto que tenga un método fly() 
+# como esrá definido en el protocolo SuperHero
+def call_a_hero(hero: SuperHero) -> None:
+    # hace una llamada a la clase superhero
+    pass
+
+class IronMan(SuperHero):
+    def fly(self) -> None:
+        print("Ironman comienza a volar")
+
+class SpiderMan:
+    def fly(self) -> None:
+        print("Spiderman comienza a volar")
+
+class Man:
+    def run(self) -> None:
+        print("Corriendo")
+
+Tony = IronMan()
+Goyo = Man()
+Peter = SpiderMan()
+
+call_a_hero(hero=Tony)
+call_a_hero(hero=Goyo)
+call_a_hero(hero=Peter)
